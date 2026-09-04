cd6f7e290e2b57e2ad8791b96dd3ae70758ba63b
Acewebs321 <acewebs321@gmail.com>
2026-09-04 14:49:15 +0100

    Add Docker and CI pipeline

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def expenses():
     return render_template('expenses.html', expenses=all_expenses)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
